0ceb62e35e884ca8b9bce8911453ce4880597c07
unknown <nikitamane2000@gmail.com>
2024-07-17 16:39:03 +0530

    ssssss

--- src/main/java/com/example/Example2/Hiii.java
+++ src/main/java/com/example/Example2/Hiii.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class Hiii {
 
-	@GetMapping("/hii")
+	@GetMapping()
 	public String hi() {
 		return "jbscs";
 	}
